84bcc65e8810dfb2e212fa711c94a077193e0fc7
quocsik46 <quocsi.a7@gmail.com>
2023-09-21 09:55:54 +0700

    server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const config = require ('./app/config');
 
 const PORT = config.app.port;
 app.listen(PORT, () => {
-    console.log('Sever is running on port ${PORT}.');
+    console.log("Sever is running on port %d.",PORT);
 });
