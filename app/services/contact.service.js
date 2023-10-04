a330422867902984fa0e544803af4d66ec964872
quocsik46 <quocsi.a7@gmail.com>
2023-10-04 19:55:07 +0700

    ket noi database

---
+++ app/services/contact.service.js
@@ -0,0 +1,8 @@
+const { ObjectId } = require("mongodb");
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts");
+    }
+}
+
+module.exports = ContactService;
